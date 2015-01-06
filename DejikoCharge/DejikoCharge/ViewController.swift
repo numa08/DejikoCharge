1983c7f41f5c2329f8e39e38f41f7b31f2a3cc44
numa08 <n511287@gmail.com>
2015-01-06 23:46:21 +0900

    create project

---
+++ DejikoCharge/DejikoCharge/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DejikoCharge
+//
+//  Created by numa08 on 2015/01/06.
+//  Copyright (c) 2015年 numanuma08. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ DejikoProvider/DejikoProvider/DejikoProvider.h
@@ -0,0 +1,19 @@
+//
+//  DejikoProvider.h
+//  DejikoProvider
+//
+//  Created by numa08 on 2015/01/06.
+//  Copyright (c) 2015年 numanuma08. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DejikoProvider.
+FOUNDATION_EXPORT double DejikoProviderVersionNumber;
+
+//! Project version string for DejikoProvider.
+FOUNDATION_EXPORT const unsigned char DejikoProviderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DejikoProvider/PublicHeader.h>
+
+
